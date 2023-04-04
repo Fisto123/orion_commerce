9ad0afcb06eb00d2d4a8f41146a699fe4e3c8c11
Fisto123 <oluwafisayoiyiola@gmail.com>
2023-04-04 18:56:39 +0100

    added stuffs

--- frontend/src/utils/newRequest.js
+++ frontend/src/utils/newRequest.js
@@ -1,9 +1,5 @@
 import axios from "axios";
 
 export const newRequest = axios.create({
-<<<<<<< HEAD
   baseURL: "https://orion-commerce.onrender.com/",
-=======
-  baseURL: "https://orion-commerce.onrender.com/"
->>>>>>> 3f22a497e3ceda06a72dd10844d628c14dbe2ac2
 });
